c818497036480a49ac095ec23ffb63f475c0dc28
Janey Slinger <janeyslinger@gmail.com>
2021-10-03 12:32:45 +0100

    add audio

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,12 +2,15 @@ extends Control
 signal disable_buttons
 
 func _ready():
-	#connect("add_ingredient", self, "_on_add_ingredient")
+	
 	pass # Replace with function body.
 
 func _on_add_ingredient(ingredient_name):
 	emit_signal("disable_buttons")
+	$Plop.play()
+	$Bubbling.play()
 	var ingredient = GlobalIngredients.get_ingredient(ingredient_name)
 	$Score.add_score(ingredient["score"])
 	yield($StabilityBar.change_stability_threshold(-ingredient["instability"]), "completed")
 	yield($StabilityBar.roll_stability(), "completed")
+	$Bubbling.stop()
