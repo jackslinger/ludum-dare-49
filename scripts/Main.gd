08feeec72bf20569e52d591801e0d5b83d70fc99
Jack Slinger <jackslinger10@gmail.com>
2021-10-03 12:31:02 +0100

    Added a finish potion button.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,5 +9,12 @@ func _on_add_ingredient(ingredient_name):
 	emit_signal("disable_buttons")
 	var ingredient = GlobalIngredients.get_ingredient(ingredient_name)
 	$Score.add_score(ingredient["score"])
-	yield($Right/StabilityBar.change_stability_threshold(-ingredient["instability"]), "completed")
-	yield($Right/StabilityBar.roll_stability(), "completed")
+	yield($StabilityBar.change_stability_threshold(-ingredient["instability"]), "completed")
+	yield($StabilityBar.roll_stability(), "completed")
+
+func _on_Finish_pressed():
+	get_tree().change_scene("res://scenes/GameOver.tscn")
+
+func _on_Failures_loose():
+	$Score.reset()
+	get_tree().change_scene("res://scenes/GameOver.tscn")
--- scripts/StabilityBar.gd
+++ scripts/StabilityBar.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 signal boil_over
 signal animation_finished
