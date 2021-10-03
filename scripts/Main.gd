29f986f129000471692cdb8bac4cd49e21706fab
Jack Slinger <jackslinger10@gmail.com>
2021-10-03 12:09:25 +0100

    Added the score and cauldron to move with the screen.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,5 +9,5 @@ func _on_add_ingredient(ingredient_name):
 	emit_signal("disable_buttons")
 	var ingredient = GlobalIngredients.get_ingredient(ingredient_name)
 	$Score.add_score(ingredient["score"])
-	yield($StabilityBar.change_stability_threshold(-ingredient["instability"]), "completed")
-	yield($StabilityBar.roll_stability(), "completed")
+	yield($Right/StabilityBar.change_stability_threshold(-ingredient["instability"]), "completed")
+	yield($Right/StabilityBar.roll_stability(), "completed")
