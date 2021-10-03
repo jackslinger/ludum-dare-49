28befc90dbe7b562c029e67b0ec8e900a22e3695
Jack Slinger <jackslinger10@gmail.com>
2021-10-03 10:54:27 +0100

    Adding a card bar to display 5 cards.

--- scripts/GlobalIngredients.gd
+++ scripts/GlobalIngredients.gd
@@ -7,119 +7,119 @@ var ingredients = [
 		"name": "scorpian_red",
 		"element": "fire",
 		"instability": 10,
-		"score": 10
+		"score": 20
 	},
 	{
 		"display_name": "Fire Ant",
 		"name": "fire_ant",
 		"element": "fire",
 		"instability": 10,
-		"score": 10
+		"score": 20
 	},
 	{
 		"display_name": "Fire Admiral Butterfly",
 		"name": "red_admiral",
 		"element": "fire",
 		"instability": 10,
-		"score": 10
+		"score": 20
 	},
 	{
 		"display_name": "Slime",
 		"name": "slime",
 		"element": "water",
-		"instability": 10,
+		"instability": 5,
 		"score": 10
 	},
 	{
 		"display_name": "Water",
 		"name": "snow",
 		"element": "water",
-		"instability": 10,
+		"instability": 5,
 		"score": 10
 	},
 	{
 		"display_name": "Ant",
 		"name": "ant",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Fly",
 		"name": "fly",
 		"element": "air",
-		"instability": 10,
-		"score": 10
+		"instability": 7,
+		"score": 15
 	},
 	{
 		"display_name": "Beak",
 		"name": "beak",
 		"element": "air",
-		"instability": 10,
-		"score": 10
+		"instability": 7,
+		"score": 15
 	},
 	{
 		"display_name": "Firefly",
 		"name": "firefly",
 		"element": "air",
-		"instability": 10,
-		"score": 10
+		"instability": 7,
+		"score": 15
 	},
 	{
 		"display_name": "Skull",
 		"name": "skull",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Bone",
 		"name": "bone",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Brain",
 		"name": "brain",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Clover",
 		"name": "clover",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Cobweb",
 		"name": "cobweb",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Eye",
 		"name": "eye",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Teeth",
 		"name": "teeth",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 2,
+		"score": 5
 	},
 	{
 		"display_name": "Tooth",
 		"name": "tooth",
 		"element": "earth",
-		"instability": 10,
-		"score": 10
+		"instability": 3,
+		"score": 8
 	},
 ]
 
--- scripts/IngredientCard.gd
+++ scripts/IngredientCard.gd
@@ -5,9 +5,15 @@ export(String) var ingredient_name
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var name = $VBoxContainer/HBoxContainer/Name
+	var element = $VBoxContainer/HBoxContainer/Element
+	var icon = $Icon
+	var instability = $VBoxContainer/HBoxContainer2/Instability
+	var score = $VBoxContainer/HBoxContainer2/Score
+	
 	var ingredient = GlobalIngredients.get_ingredient(ingredient_name)
-	$Name.text = ingredient["display_name"]
-	$Element.texture = load("res://assets/simple_elements/" + ingredient["element"] + ".png")
-	$Icon.texture = load("res://assets/ingredients/" + ingredient_name + ".png")
-	$Instability.text = "Instability: " + str(ingredient["instability"])
-	$Score.text = "Score: " + str(ingredient["score"])
+	name.text = ingredient["display_name"]
+	element.texture = load("res://assets/simple_elements/" + ingredient["element"] + ".png")
+	icon.texture = load("res://assets/ingredients/" + ingredient_name + ".png")
+	instability.text = "Instability: " + str(ingredient["instability"])
+	score.text = "Score: " + str(ingredient["score"])
